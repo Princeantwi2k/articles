7fcde50a2b6889532d6f1d0ab1675a0f102f36ce
princeantwi2k <antwi2k@gmail.com>
2021-09-04 20:15:36 +0800

    added Homepage

--- src/Component/Movielist.jsx
+++ src/Component/Movielist.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Movielist = (props) => {
+const Movielist = ({ movies }) => {
   return (
     <>
-      {props.movies.map((movies, index) => (
-        <div key={index} className="d-flex justify-content-start m-3 ">
-          <Card className="card">
-            <img
-              src={movies.multimedia.src}
-              alt="pic"
-              className="img-responsive img"
-            />
-            <h5>
-              {" "}
-              <span style={{ color: `orangered` }}>title</span> :{" "}
-              {movies.display_title}
-            </h5>
-            <h5>
-              <span style={{ color: `aqua` }}>headline </span>:{" "}
-              {movies.headline}
-            </h5>
-            <h5>
-              <span style={{ color: `orangered` }}>summary_short</span> :{" "}
-              {movies.summary_short}
-            </h5>
-            <h5>
-              {" "}
-              <span style={{ color: `aqua` }}>publication_date</span>:{" "}
-              {movies.publication_date}
-            </h5>
-          </Card>
-        </div>
+      {movies.map((movies, id) => (
+        <Link to={`/blog/${movies.id}`} className="links">
+          <div key={id}>
+            <Card className="card">
+              <img src={movies.src} alt="pic" className="img-responsive img" />
+              <h5>
+                {" "}
+                <span style={{ color: `orangered` }}>title</span> :{" "}
+                {movies.display_title}
+              </h5>
+              <h5>
+                <span style={{ color: `aqua` }}>headline </span>:{" "}
+                {movies.headline}
+              </h5>
+              <h5>
+                <span style={{ color: `orangered` }}>summary_short</span> :{" "}
+                {movies.summary_short}
+              </h5>
+              <h5>
+                {" "}
+                <span style={{ color: `aqua` }}>publication_date</span>:{" "}
+                {movies.publication_date}
+              </h5>
+            </Card>
+          </div>
+        </Link>
       ))}
-      <button>
-        <Link to="/home">Home</Link>
-      </button>
     </>
   );
 };
 
-export default Movielist;
+const mapStateToProps = (state) => {
+  return { movies: state.movies };
+};
+
+export default connect(mapStateToProps)(Movielist);
