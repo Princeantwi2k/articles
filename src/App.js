cfb22a972f05c0c94384dfd457bd8eb334e81422
princeantwi2k <antwi2k@gmail.com>
2021-09-04 11:22:31 +0800

    added initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Homepage from './Component/Homepage';
-
+import {BrowserRouter ,Switch,Route} from 'react-router-dom'
 class App extends Component {
   render() {
     return (
       <div>
-        <Homepage />
+        <BrowserRouter>
+        
+        <Route path="/home" exact component={Homepage} />
+        </BrowserRouter>
+     
       </div>
     );
   }
