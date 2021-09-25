7f1d00fa1736e2b86cf3bcee0187843e554e300a
princeantwi2k <antwi2k@gmail.com>
2021-09-25 14:34:48 +0800

    added sciencelist.jsx

---
+++ src/Component/Sciencelist.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Tech.css";
+
+const Sciencelist = (props) => {
+  return (
+    <>
+      {props.science.map((science, idx) => (
+        <div key={idx}>
+          <img src={science.multimedia?.[0].url} alt="pic" className="pic" />
+          <h5 className="text">
+            <span style={{ color: "red" }}>Title:</span> {science.title}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>abstract :</span>
+            {science.abstract}
+          </h5>
+          <h5 className="text">
+            {" "}
+            <span style={{ color: "red" }}>byline: </span>
+            {science.byline}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>published_date :</span>{" "}
+            {science.published_date}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>item_type :</span>{" "}
+            {science.item_type}
+          </h5>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Sciencelist;
