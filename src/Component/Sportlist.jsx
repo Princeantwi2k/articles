7d79d1c982f8b177d4ab60afff72a552871fe56f
princeantwi2k <antwi2k@gmail.com>
2021-09-25 14:35:22 +0800

    added sportlist.jsx

---
+++ src/Component/Sportlist.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Tech.css";
+
+const Sportlist = (props) => {
+  return (
+    <>
+      {props.sport.map((sport, idx) => (
+        <div key={idx}>
+          <img src={sport.multimedia?.[0].url} alt="pic" className="pic" />
+          <h5 className="text">
+            <span style={{ color: "red" }}>Title:</span> {sport.title}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>abstract :</span>
+            {sport.abstract}
+          </h5>
+          <h5 className="text">
+            {" "}
+            <span style={{ color: "red" }}>byline: </span>
+            {sport.byline}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>published_date :</span>{" "}
+            {sport.published_date}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>item_type :</span> {sport.item_type}
+          </h5>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Sportlist;
