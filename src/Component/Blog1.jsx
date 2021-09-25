9c3504b94a4288ca9b6e03a9882052d33301deae
princeantwi2k <antwi2k@gmail.com>
2021-09-25 14:04:20 +0800

    added blog

--- src/Component/Blog1.jsx
+++ src/Component/Blog1.jsx
@@ -1,50 +1,30 @@
-import { useParams } from "react-router-dom";
 import React from "react";
-import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
 import { Container, Col, Row } from "react-bootstrap";
-import "./Blog.css";
 
-const Blog1 = ({ favorite }) => {
+const Blog1 = () => {
   const { id } = useParams();
-
-  let [product] = favorite.filter((favorite) => favorite.id === id);
-
+  let [product] = props.business.filter((business) => business.id === id);
   return (
-    <>
-      <div className=" d-grid gap-2">
-        <Container>
-          <Row>
-            <Col sm={4} md={4} className="items-img">
-              <img src={product.src} alt="pic" className="picture" />
-            </Col>
-            <Col sm={8} md={8} className="text">
-              <h5>
-                {" "}
-                <span style={{ color: `red` }}>title</span> :{" "}
-                {product.display_title}
-              </h5>
-              <h5>
-                <span style={{ color: `yellow` }}>headline </span>:{" "}
-                {product.headline}
-              </h5>
-              <h5>
-                <span style={{ color: `yellow` }}>summary_short</span> :{" "}
-                {product.summary_short}
-              </h5>
-              <h5>
-                {" "}
-                <span style={{ color: `green` }}>publication_date</span>:{" "}
-                {product.publication_date}
-              </h5>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </>
+    <div className="bloglist d-grid gap-2">
+      <Container>
+        <Row>
+          <Col sm={4} md={4} className="items-img">
+            <img
+              src={product.multimedia?.[0].url}
+              alt="pic"
+              className="item img-responsive"
+            />
+          </Col>
+          <Col sm={8} md={8}>
+            <h1 className="items-name">{product.title}</h1>
+            <p>{product.abtract}</p>
+            <Example />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
-const mapStateToprops = (state) => {
-  return { favorite: state.favorite };
-};
 
-export default connect(mapStateToprops)(Blog1);
+export default Blog1;
