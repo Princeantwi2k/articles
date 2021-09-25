3493e4f56b7e71fb623823c745fb587d9dd0312c
princeantwi2k <antwi2k@gmail.com>
2021-09-25 14:34:10 +0800

    added Politicslist.jsx

---
+++ src/Component/Politicslist.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Tech.css";
+
+const Politicslist = ({ politics }) => {
+  return (
+    <>
+      {politics.map((politics, idx) => (
+        <div key={idx}>
+          <img src={politics.multimedia?.[0].url} alt="pic" className="pic" />
+          <h5 className="text">
+            <span style={{ color: "red" }}>Title:</span> {politics.title}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>abstract :</span>
+            {politics.abstract}
+          </h5>
+          <h5 className="text">
+            {" "}
+            <span style={{ color: "red" }}>byline: </span>
+            {politics.byline}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>published_date :</span>{" "}
+            {politics.published_date}
+          </h5>
+          <h5 className="text">
+            <span style={{ color: "red" }}>item_type :</span>{" "}
+            {politics.item_type}
+          </h5>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Politicslist;
