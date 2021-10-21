e7a0f61dfc4825c11d0001684c71e84d44b0370d
princeantwi2k <antwi2k@gmail.com>
2021-10-21 12:36:52 +0800

    added boostrap

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -5,53 +5,91 @@ import "./Nav.css";
 const Nav = () => {
   return (
     <div>
-      <nav>
-        <header>
-          <div className="logo">
-            Filler <span style={{ color: "red" }}>Hub</span>
-          </div>
-          <div>
-            <ul>
-              <li>
-                <Link to="/" className="links">
-                  Magazine
-                </Link>
-              </li>
-              <li>
+      <div className="container-fluid ">
+        <nav className="navbar navbar-default navbar-fixed-top navbar-home">
+          <div className="container">
+            <div className="navbar-header">
+              <button
+                type="button"
+                className="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <h6>
                 {" "}
-                <Link to="/sport" className="links">
+                <span
+                  style={{
+                    color: "white",
+                    fontSize: "20px",
+                    fontWeight: "900px",
+                  }}
+                >
+                  Filler
+                </span>{" "}
+                <span
+                  style={{
+                    color: "red",
+                    fontSize: "30px",
+                    fontWeight: "900px",
+                  }}
+                >
+                  Hub
+                </span>
+              </h6>
+            </div>
+
+            <div
+              className="collapse navbar-collapse nav-button"
+              id="bs-example-navbar-collapse-1"
+            >
+              <ul className="nav navbar-nav navbar-right nav-button">
+                <li>
+                  <Link to="/" className="links">
+                    Magazine
+                  </Link>
+                </li>
+                <li>
                   {" "}
-                  Sport
-                </Link>{" "}
-              </li>
-              <li>
-                {" "}
-                <Link to="/tech" className="links">
-                  Technology
-                </Link>{" "}
-              </li>
-              <li>
-                {" "}
-                <Link to="/science" className="links">
-                  Science
-                </Link>{" "}
-              </li>
-              <li>
-                {" "}
-                <Link to="/poli" className="links">
-                  Politics
-                </Link>
-              </li>
-              <li>
-                {" "}
-                <Link to="/business" className="links">
-                  Business
-                </Link>{" "}
-              </li>
-            </ul>
+                  <Link to="/sport" className="links">
+                    {" "}
+                    Sport
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/tech" className="links">
+                    Technology
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/science" className="links">
+                    Science
+                  </Link>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/poli" className="links">
+                    Politics
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link to="/business" className="links">
+                    Business
+                  </Link>{" "}
+                </li>
+              </ul>
+            </div>
           </div>
-        </header>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 };
